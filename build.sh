8be303af2f1b5f3b432ce09325b2395e4332a358
duckviet <ducnguyenkudo@gmail.com>
2025-12-01 20:21:51 +0700

    Fix: Heroku Go buildpack detection, update build script

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ set -e
 echo "Building Go application..."
 cd backend
 
+go mod tidy
+go mod download
 # Build the application
 go build -o bin/gin-collaborative-editor-backend ./cmd/app
 
---
+++ go.mod
@@ -0,0 +1,2 @@
+module heroku.app/main
+go 1.24.0
