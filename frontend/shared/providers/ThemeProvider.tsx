58434c61cb4ec1f64714e4e9a271f6fde4a907c7
duckviet <ducnguyenkudo@gmail.com>
2025-12-25 23:33:43 +0700

    feat: integrate bundle analyzer and update ThemeProvider props typing

--- frontend/shared/providers/ThemeProvider.tsx
+++ frontend/shared/providers/ThemeProvider.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: React.PropsWithChildren<React.ComponentProps<typeof NextThemesProvider>>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
-
